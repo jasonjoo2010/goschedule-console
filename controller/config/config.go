e70cd158cb9c1e2d7c94d61a0f93f99c52f2ae32
Jason Joo <hblzxsj@163.com>
2020-05-15 18:29:48 +0800

    Complete Import/Export/Dump pages

--- controller/config/config.go
+++ controller/config/config.go
@@ -1,13 +1,20 @@
 package config
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jasonjoo2010/goschedule-console/app"
 	"github.com/jasonjoo2010/goschedule-console/controller"
 	"github.com/jasonjoo2010/goschedule-console/types"
 	"github.com/jasonjoo2010/goschedule-console/utils"
+	"github.com/jasonjoo2010/goschedule/core/definition"
+)
+
+const (
+	PREFIX_STRATEGY = "Strategy:"
 )
 
 func Init(engine *gin.Engine) {
@@ -15,6 +22,10 @@ func Init(engine *gin.Engine) {
 
 	configGroup.GET("/modify", modifyHandler)
 	configGroup.POST("/save", saveHandler)
+	configGroup.GET("/dump", dumpHandler)
+	configGroup.GET("/export", exportHandler)
+	configGroup.GET("/import", importHandler)
+	configGroup.POST("/importSave", importSaveHandler)
 }
 
 func saveHandler(c *gin.Context) {
@@ -68,4 +79,78 @@ func modifyHandler(c *gin.Context) {
 		"password":  conf.Password,
 		"namespace": conf.Namespace,
 	}))
+}
+
+func dumpHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "config/dump.html", controller.DataWithSession(gin.H{
+		"content": app.Instance().Store.Dump(),
+	}))
+}
+
+func exportHandler(c *gin.Context) {
+	store := app.Instance().Store
+	strategies, _ := store.GetStrategies()
+	b := &strings.Builder{}
+	for _, s := range strategies {
+		b.WriteString("Strategy:")
+		data, _ := json.Marshal(s)
+		b.Write(data)
+		b.WriteString("\n")
+	}
+	// TODO tasks
+	c.HTML(http.StatusOK, "config/export.html", controller.DataWithSession(gin.H{
+		"exported": b.String(),
+	}))
+}
+
+func importHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "config/import.html", controller.DataWithSession(gin.H{}))
+}
+
+func parseImportContent(content string) []*definition.Strategy {
+	strategies := make([]*definition.Strategy, 0, 2)
+	lines := strings.Split(content, "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		if strings.Index(line, PREFIX_STRATEGY) == 0 {
+			// strategy
+			jsonStr := line[len(PREFIX_STRATEGY):]
+			strategy := definition.Strategy{}
+			err := json.Unmarshal([]byte(jsonStr), &strategy)
+			if err != nil {
+				continue
+			}
+			strategies = append(strategies, &strategy)
+		}
+	}
+	return strategies
+}
+
+func importSaveHandler(c *gin.Context) {
+	msg := types.NewEmptyResponse()
+	defer c.JSON(200, msg)
+	content := c.PostForm("content")
+	if content == "" {
+		msg.Err(1, "Empty importing content")
+		return
+	}
+	strategies := parseImportContent(content)
+	strategiesTotal := len(strategies)
+	strategiesSuccess := 0
+	store := app.Instance().Store
+	for _, strategy := range strategies {
+		s, _ := store.GetStrategy(strategy.Id)
+		if s != nil {
+			continue
+		}
+		err := store.CreateStrategy(strategy)
+		if err != nil {
+			continue
+		}
+		strategiesSuccess++
+	}
+	msg["strategiesTotal"] = strategiesTotal
+	msg["strategiesSuccess"] = strategiesSuccess
 }
